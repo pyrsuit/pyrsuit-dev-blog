ae0effd888d3286e19b5eeb56f2b3766737149b2
zsofiak96 <kalosi.zsofia@gmail.com>
2025-06-14 16:42:12 +0200

    WIP

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/pyrsuit-dev-blog/' : '/blog',
+			base: process.env.NODE_ENV === 'production' ? '/pyrsuit-dev-blog' : '/blog'
         }
 	}
 };
