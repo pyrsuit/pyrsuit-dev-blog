4ef5cab600de77d7fb7c028ccd5e9b335eca18ff
zsofiak96 <kalosi.zsofia@gmail.com>
2025-06-14 16:40:52 +0200

    fix: base should point to project name
    NOT path

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-            base: '/blog'
+			base: process.env.NODE_ENV === 'production' ? '/pyrsuit-dev-blog' : '/blog'
         }
 	}
 };
