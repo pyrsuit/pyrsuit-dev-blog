4c203fc1ee7ffdd60fe406013f2254709c463949
zsofiak96 <kalosi.zsofia@gmail.com>
2025-07-06 08:14:21 +0200

    style: Highlight relevant text elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 
 <main class="min-h-screen flex flex-col items-center justify-center bg-yellow-50 text-black px-4">
   <h1 class="text-6xl font-extralight tracking-wide text-center" style="font-family: 'Montserrat', sans-serif;">
-    The Py(thon Pu)rsuit
+  The Py<span style="font-weight: 100;">(thon Pu)</span>rsuit
   </h1>
-  <p class="text-2xl text-center mt-4 tracking-wider" style="font-family: 'Montserrat', sans-serif;">
+<p class="text-2xl text-center mt-4 tracking-wider" style="font-family: 'Montserrat', sans-serif; font-weight: 100;">
     coming soon
   </p>
 </main>
