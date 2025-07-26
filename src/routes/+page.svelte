b2ac5c7a4700c97d7f47f87c0e52f43089607b4c
zsofiak96 <kalosi.zsofia@gmail.com>
2025-07-26 20:37:30 +0200

    feat: Remove coming soon flag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   The Py<span style="font-weight: 100;">(thon Pu)</span>rsuit
 </h1>
 <p class="text-2xl text-center mt-4 tracking-wider" style="font-family: 'Montserrat', sans-serif; font-weight: 100;">
-  coming soon
+  is here
 </p>
 
 <style lang="postcss">
