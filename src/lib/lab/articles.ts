d0c125ef0108262595d8ecfbd1db1d1975715ca8
zsofiak96 <kalosi.zsofia@gmail.com>
2026-04-26 11:03:37 +0200

    feat: Publish new article no gil no cry

--- src/lib/lab/articles.ts
+++ src/lib/lab/articles.ts
@@ -1,5 +1,10 @@
 export const articles = [
 	{
+		slug: 'no_gil_no_cry',
+		title: 'No GIL, no cry?',
+		date: 'April 26, 2026'
+	},
+	{
 		slug: 'a_cookbook_for_a_weather_station_part_2',
 		title: 'A cookbook for a weather station — Part II',
 		date: 'September 6, 2025'
