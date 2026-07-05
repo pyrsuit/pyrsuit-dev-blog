052ece57c60b7f75b36f8df98c841d1ba8bb5ab0
pyrsuit <pyrsuit@users.noreply.github.com>
2026-07-05 11:38:28 +0000

    deploy: 1e2b1b5cfbc6c4d2980106ac09ce2df6f7b06926

--- _app/immutable/nodes/5.B8FdbQVH.js
+++
@@ -1,1 +0,0 @@
-import{bC as e,bB as n}from"../chunks/Cg8tJacl.js";export{e as component,n as universal};
---
+++ _app/immutable/nodes/5.CIaDcl9U.js
@@ -0,0 +1,1 @@
+import{bC as e,bB as n}from"../chunks/B65p-Wka.js";export{e as component,n as universal};
