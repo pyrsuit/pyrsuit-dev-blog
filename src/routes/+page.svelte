463ad90753650d37f50bc027ce441ee20fa13638
zsofiak96 <kalosi.zsofia@gmail.com>
2026-07-17 21:09:01 +0200

    refactor: Add new ABout description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1 >
   The Py<span style="font-weight: 100;">(thon Pu)</span>rsuit
 </h1>
-<p class="font-extralight text-base text-gray-600">Everything a Python developer cares about.</p>
+<p class="font-extralight text-base text-gray-600">All things Python.</p>
