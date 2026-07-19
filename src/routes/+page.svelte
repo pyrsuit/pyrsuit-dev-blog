3e6ae712c7f6546d1ea086d4d319161937b58bf8
zsofiak96 <kalosi.zsofia@gmail.com>
2026-07-19 09:09:33 +0200

    feat: Make Python on about page bold

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1 >
   The Py<span style="font-weight: 100;">(thon Pu)</span>rsuit
 </h1>
-<p class="font-extralight text-base text-gray-600">All things Python.</p>
+<p class="font-extralight text-base text-gray-600">All things <span class="text-gray-900">Python</span>.</p>
